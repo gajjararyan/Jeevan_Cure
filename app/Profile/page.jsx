d7cb9b139773bd6378ed193538031a5e6cb995b3
Khushal <khandelwalkhushal79@gmail.com>
2024-02-11 06:44:05 +0530

    Refactor

--- app/Profile/page.jsx
+++ app/Profile/page.jsx
@@ -1,12 +1,9 @@
 "use client";
 
 import ProfileApp from "./ProfileApp";
-import RootLayout from '@/app/Dashboard/layout';
 
 export default function Profile() {
   return (
-    <RootLayout>
       <ProfileApp />
-    </RootLayout>
   );
 }
